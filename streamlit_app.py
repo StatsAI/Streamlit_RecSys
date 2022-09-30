654d42788974843b6554159a471a3810b626a355
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-09-30 03:45:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ image = Image.open('images/rec_sys.PNG')
 
 st.image(image)
 
-st.sidebar.write('Instructions: Use the below controls to select the number of movie recommendations you would like to generate for a given user.')
+st.sidebar.write('Instructions: Use the below controls to select the number of movie recommendations you would like to generate for a given user. 🚀')
 
 user_id = st.sidebar.selectbox(label = 'Select the user ID', options = ('1', '2', '3', '4', '5')) 
 
