7728d5feeef7964d76883f708acb72164608f04f
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-09-30 01:50:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,12 @@ st.write('This is a web app to recommend movies using the Tensorflow Recommender
 
 st.write('Instructions: The inputs on the sidebar allow you to select the user and the number of recommendations you would like to get. After that, click on the generate recommendations button at the bottom to get the results!')
 
+
+num_recs = st.sidebar.slider(label = 'Number of Recommendations', min_value = 1.0,
+                          max_value = 5.0 ,
+                          value = 3.0,
+                          step = 1.0)
+
 fixed_acidity = st.sidebar.slider(label = 'Fixed Acidity', min_value = 4.0,
                           max_value = 16.0 ,
                           value = 10.0,
