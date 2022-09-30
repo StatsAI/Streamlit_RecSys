a45b304fb9eaee2a5709cfc1d1a77545cfa2ee06
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-09-30 16:28:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ from PIL import Image
 #loaded_ranking_model = tf.saved_model.load('models/basic_ranking_model.pb')
 
 # this does not work
-loaded_retrieval_model = tf.saved_model.load('models/basic_retrieval_model.pb')
+#loaded_retrieval_model = tf.saved_model.load('models/basic_retrieval_model.pb')
 
 # this does not work
-#loaded_retrieval_model = tf.keras.models.load_model('models/basic_retrieval_model.pb')
+loaded_retrieval_model = tf.keras.models.load_model('models/basic_retrieval_model.pb')
 
 # this works
 #model1 = tf.keras.models.load_model('models/my_keras_model1.h5')
