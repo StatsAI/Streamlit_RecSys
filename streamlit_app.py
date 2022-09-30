6e5a04c9511e9c41a46ee9601c130d0dbcfd0b9b
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-09-30 03:00:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title('Tensorflow Recommenders Library Movie Recommendation System')
 
 st.write('This is a web app to recommend movies to users based upon their watch history using the Tensorflow Recommenders Python library.')
 
-st.write('It uses the following approach, as outlined by: [Tensorflow Recommenders](https://www.tensorflow.org/recommenders/examples/basic_retrieval)')         
+st.write('It uses the following two-model approach approach, as outlined by: [Tensorflow Recommenders](https://www.tensorflow.org/recommenders/examples/basic_retrieval)')         
          
 #opening the image
 
@@ -17,8 +17,12 @@ image = Image.open('images/rec_sys.PNG')
 
 st.image(image)
 
-st.write('Instructions: The inputs on the sidebar allow you to select the user and the number of recommendations you would like to generate for the user. /nClick on the generate recommendations button to get the results!')
+st.write('Instructions: The inputs on the sidebar allow you to select the user and the number of recommendations you would like to generate for the user')
 
+st.write('Click on the generate candidates button to generate a list of candidates using the retreval model.')
+
+st.write('Click on the rank candidates button to rank the candidates using the ranking model.')
+         
 user_id = st.sidebar.selectbox(label = 'Select the user ID', options = ('1', '2', '3', '4', '5')) 
 
 num_recs = st.sidebar.slider(label = 'Number of Recommendations', min_value = 1.0,
