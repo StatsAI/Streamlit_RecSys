2a7e4c02694a3b63f69b2c534bfe2e19cec40f20
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-09-30 16:12:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,10 @@ from PIL import Image
 #loaded_retrieval_model = tf.saved_model.load('models/basic_retrieval_model.pb')
 #loaded_ranking_model = tf.saved_model.load('models/basic_ranking_model.pb')
 
-loaded_retrieval_model = tf.keras.models.load_model('models/basic_retrieval_model.pb')
+#loaded_retrieval_model = tf.keras.models.load_model('models/basic_retrieval_model.pb')
 
-#model1 = tf.keras.models.load_model('models/my_keras_model1.h5')
+model1 = tf.keras.models.load_model('models/my_keras_model1.h5')
+#model1 = tf.saved_model.load('models/my_keras_model1.h5')
 
 
 ####################################################################################################################################################
