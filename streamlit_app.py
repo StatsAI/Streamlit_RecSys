ad165a564066a479e4f6893b26e4b0ca3b775e21
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-10-01 15:48:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ def ranking_predict(num_recs, user_id, candidate_predictions):
 	
 	for title, score in sorted(result.items(), key=lambda x: x[1], reverse=True):	
 		#result[movie_title] = [title, score]
-		result[movie_title] = [title, score.numpy()]
+		result[movie_title] = [title, score.numpy()[0]]
 
 	#result = result.numpy()[0]
 	
