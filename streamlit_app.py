0602384fe8c6ca8640bb40b55850776c57009d07
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-10-01 17:27:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def retrieval_predict(num_recs, user_id):
 		title = str(title)
 		title = title.replace('b', '')
 		title = title.strip()
-		title = title.replace('"', "")
+		#title = title.replace('"', "")
 		title = title.replace("'","")
 		holder.append(title)		
 	
