53232271b456279f8d4ada788e8907d72b0aa36f
StatsAI <67183539+StatsAI@users.noreply.github.com>
2022-09-30 21:55:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def retrieval_predict(num_recs, user_id):
 	
 	scores, titles = loaded_retrieval_model([user_id])
 	
-	return titles[0][:num_recs]
+	return list(titles[0])[:num_recs]
 
 	#return loaded_retrieval_model
 
